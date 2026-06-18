fdcc88a60dc1d69328b7456c7d01f45ade296fbe
westernnationalauthority-beep <westernnationalauthority@gmail.com>
2026-06-18 17:10:05 +0200

    Fix formatting of API_URL export statement

--- src/data/employees.ts
+++ src/data/employees.ts
@@ -27,7 +27,7 @@ export interface Employee {
   gender: string;
 }
 
-export const API_URL = "https://script.google.com/macros/s/AKfycbwVhs0wu_ddNOC58MkPDl1SvC5_jo8I9YGYSebHf4k6y68Lnj9nEDOUAOk1RKiRrr5P/exec";
+export const API_URL ="https://script.google.com/macros/s/AKfycbwVhs0wu_ddNOC58MkPDl1SvC5_jo8I9YGYSebHf4k6y68Lnj9nEDOUAOk1RKiRrr5P/exec";
 export const INITIAL_CODE = "NACC2026";
 
 /* ============================================================
